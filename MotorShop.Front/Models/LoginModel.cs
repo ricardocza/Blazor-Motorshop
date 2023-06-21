bceaff865ea770457a304244417d530e1128bdec
ricardocza <ricardovcza@gmail.com>
2023-06-21 17:16:51 -0300

    initial commit

---
+++ MotorShop.Front/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotorShop.Front.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "This is not a valid email!")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "This password is too short!")]
+        public string Password { get; set; }
+    }
+}
---
+++ MotorShop.Front/Models/RegisterModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MotorShop.Front.Models
+{
+    public class RegisterModel
+    {
+        public int Id { get; set; }
+        
+        [Required]        
+        public string FirstName { get; set; }
+        
+        [Required]        
+        public string LastName { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}
